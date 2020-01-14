8410a2a67586c80f489eeb8193abd953ba9a2a39
yc111 <yinchuan_111@163.com>
2020-01-15 01:16:19 +0800

    write filelist plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const MyPlugin = require('./plugins/MyPlugin');
+const FileListPlugin = require('./plugins/FileListPlugin');
 
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js'
+        index: './src/index.js',
+        a: './src/a.js'
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new MyPlugin()
+        new MyPlugin(),
+        new FileListPlugin({
+            filename: '_filelist.md'
+        })
     ]
 }
